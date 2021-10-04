96ac213cf04e0888432c415b13f7fbf284ff7220
minseokLim <mslim8803@naver.com>
2021-10-05 01:27:53 +0900

    test: add all possible test cases

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -39,6 +39,66 @@ public class ApplicationTest extends NSTest {
 		}
 	}
 
+	@Test
+	void 유효하지_않은_사용자_입력() {
+		try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+			mockRandoms.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+				.thenReturn(7, 1, 3);
+			running("1234", "109", "131", "713", "3");
+			verify("[ERROR]", "3자리 수를 입력해주세요.", "1에서 9까지의 수만 입력해주세요.",
+				"서로 다른 수를 입력해주세요.", "게임 종료 여부를 정하기 위해 1과 2만 입력해주세요.");
+		}
+	}
+
+	@Test
+	void 원볼() {
+		testSingleMatchResult("246", "1볼");
+	}
+
+	@Test
+	void 투볼() {
+		testSingleMatchResult("216", "2볼");
+	}
+
+	@Test
+	void 쓰리볼() {
+		testSingleMatchResult("231", "3볼");
+	}
+
+	@Test
+	void 원스트라이크() {
+		testSingleMatchResult("145", "1스트라이크");
+	}
+
+	@Test
+	void 원스트라이크_원볼() {
+		testSingleMatchResult("152", "1스트라이크 1볼");
+	}
+
+	@Test
+	void 원스트라이크_투볼() {
+		testSingleMatchResult("132", "1스트라이크 2볼");
+	}
+
+	@Test
+	void 투스트라이크() {
+		testSingleMatchResult("126", "2스트라이크");
+	}
+
+	@Test
+	void 쓰리스트라이크() {
+		testSingleMatchResult("123", "3스트라이크");
+	}
+
+	private void testSingleMatchResult(final String input, final String output) {
+		try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+			mockRandoms.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+				.thenReturn(1, 2, 3);
+			running(input);
+			verify(output);
+		}
+	}
+
 	@AfterEach
 	void tearDown() {
 		outputStandard();
